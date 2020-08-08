04478e92e62c0bf4a806ecec7271dcd64f4981d0
Nhanbn98 <nhann98@gmail.com>
2020-08-08 21:44:36 +0700

    fix-config

--- src/main/java/com/example/sell/config/WebSecurityConfig.java
+++ src/main/java/com/example/sell/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable();
+        http.cors();
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()

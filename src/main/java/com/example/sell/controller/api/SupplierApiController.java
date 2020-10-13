3f1e04ca250d1f0a576b81f5975e3724effb9b22
Nhanbn98 <nhanbn98@gmail.com>
2020-10-13 12:34:44 +0700

    up

--- src/main/java/com/example/sell/controller/api/SupplierApiController.java
+++ src/main/java/com/example/sell/controller/api/SupplierApiController.java
@@ -87,13 +87,13 @@ public class SupplierApiController {
              supplier1.setLogo(supplier.getLogo());
              supplier1.setStatus(supplier.getStatus());
             String idCode =  supplierService.getLastSupplier().getIdCode();
-            String stringNumber = idCode.substring(4,9);
-            int idCodeNumber = Integer.parseInt(stringNumber);
-            idCodeNumber++;
-            String numberCode =  String.format("%05d%n",idCodeNumber);
-            String fix = "SUPP";
-            String newIdCode = fix.concat(numberCode).replace(" ","");
-            supplier1.setIdCode(newIdCode);
+                String stringNumber = idCode.substring(4, 9);
+                int idCodeNumber = Integer.parseInt(stringNumber);
+                idCodeNumber++;
+                String numberCode = String.format("%05d%n", idCodeNumber);
+                String fix = "SUPP";
+                String newIdCode = fix.concat(numberCode);
+                supplier1.setIdCode(newIdCode);
 
             supplierService.addNewSupplier(supplier1);
             result.setSuccess(true);
